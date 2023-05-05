6f78490b93cc4d85eec63646d8bca2baeb98c23d
Mauro <mauro.henriquegdecarvalho@gmail.com>
2023-05-05 11:37:58 -0300

    projeto final (resolvendo problema no push)

---
+++ src/main/java/com/example/demo/RestApiProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiProjetoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/Usuario.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long id;
+	String nome;
+	String sobrenome;
+	String email;
+	Sexo sexo;
+	String cpf;
+	String ddd;
+	String telefone;
+	NivelDeAcesso nivelDeAcesso;
+	
+	
+}
